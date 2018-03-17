3ffaa528c8af07676defc96df627ab7672722153
José Victor <josevictorpatos@gmail.com>
2018-03-17 18:33:30 -0300

    merge filters

--- source/core.R
+++ source/core.R
@@ -8,12 +8,13 @@ m_e <- as.data.frame.matrix(read.table(path, header = T, sep = ";"))
 
 #m_e <- as_data_frame(m_e)
 
-filteredm_e <- filter(m_e, co_grupo == 22)
-
 #https://blogs.sas.com/content/iml/2015/08/31/digits-in-integer.html
-filteredm_e <- filter(filteredm_e, ceiling(log10(vt_ace_oce + 1)) == 27)
+filteredm_e <- filter(m_e, co_grupo == 22, ceiling(log10(vt_ace_oce + 1)) == 27)
 
 #http://rprogramming.net/write-csv-in-r/
 write.csv(filteredm_e, file = "result/mydata.csv", row.names = FALSE, na = "")
 
 
+# mat <- matrix(, nrow = 23996, ncol = 28)
+# for(column in 1:n.columns){
+#   mat[, column] <- vector
